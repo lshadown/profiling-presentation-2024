20141295ae4273e93eb257ab6aaeba1a7e77ebd7
Mateusz Grużewski <mateusz.gruzewski@assecods.pl>
2024-03-03 10:56:52 +0100

    init

---
+++ src/main/java/com/gruzewskidev/profiling/ProfilingApplication.java
@@ -0,0 +1,13 @@
+package com.gruzewskidev.profiling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfilingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfilingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gruzewskidev/profiling/ProfilingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gruzewskidev.profiling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfilingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
